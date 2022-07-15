4ad656a210b041897edc478adaee748c4be6eb37
Julian Boardman <juliandboardman@gmail.com>
2022-07-15 06:01:18 -0500

    Dealing Damage

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+
+    private int _currentHealth;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _currentHealth = maxHealth;
+    }
+
+    public void DealDamage(int damage)
+    {
+        if (_currentHealth <= 0) { return; }
+        _currentHealth = Mathf.Max(_currentHealth - damage, 0);
+        print(_currentHealth);
+    }
+
+}
---
+++ Assets/Scripts/Combat/WeaponDamage.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDamage : MonoBehaviour
+{
+    [SerializeField] private Collider myCollider;
+    private List<Collider> _alreadyCollidedWith = new List<Collider>();
+
+    private void OnEnable()
+    {
+        _alreadyCollidedWith.Clear();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(gameObject.tag)) { return; }
+        if (_alreadyCollidedWith.Contains(other)) { return; }
+        _alreadyCollidedWith.Add(other);
+        if (other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(10);
+        }
+    }
+}
